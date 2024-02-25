7b342bf85e9bf205629a7c6bd933b7fcbd3a1bb6
zaqbgtmlp1 <letuan32334@gmail.com>
2024-02-26 05:50:36 +0700

    first conmmit

---
+++ AppQuanLyCyberGame/AppQuanLyCyberGame/ViewModel/MainViewModel.cs
@@ -0,0 +1,45 @@
+﻿using AppQuanLyCyberGame.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace AppQuanLyCyberGame.ViewModel
+{
+    public class MainViewModel : BaseViewModel
+    {
+        // mọi thứ xử lý sẽ nằm trong này
+
+        public bool Isloaded = false;
+        public MainViewModel()
+        {
+            if(!Isloaded)
+            {
+                Isloaded = true;
+                LoginWindow loginWindow = new LoginWindow();
+                LoadClientComputer();
+                loginWindow.ShowDialog();
+            }
+
+            ClientComputer cl1 = new ClientComputer()
+            {
+                Id = 3,
+                CCStatus = false,
+                CCType = 1,
+            };
+
+
+
+
+
+        }
+
+        void LoadClientComputer()
+        {
+            var Client1 = DataProvider.Ins.DB.Users;
+
+        }
+    }
+}
